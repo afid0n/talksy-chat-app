92a98637a74d5e65c144145856865faff57c372f
qasimovemil <emileg@code.edu.az>
2025-07-20 06:09:24 +0400

    feat(ui): add animated 3D particle background using three.js in React

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,16 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ROUTES from "./routes";
+import ThreeBackground from "./components/three-background/ThreeBackground";
 const router = createBrowserRouter(ROUTES);
 
 
 function App() {
   return (
+    <>
+     <ThreeBackground />
       <RouterProvider router={router} />
+    </>
+   
   );
 }
 
