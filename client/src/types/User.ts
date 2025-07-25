74dfe9420e5da0826d19958afc344217dcbc0587
afid0n <fidanza@code.edu.az>
2025-07-25 15:35:02 +0400

    update

--- client/src/types/User.ts
+++ client/src/types/User.ts
@@ -1,52 +1,36 @@
 export interface User {
-    id: string ; 
-    email: string  
-    fullName: string;
-    username: string;
-    authProvider: 'google' | 'github' | 'local';
-    password?: string; // optional in client (usually hidden anyway)
-    birthday?: string | null;
-    avatar: {
-        url: string;
-        public_id?: string;
-    };
-    location: {
-        country?: string;
-        city?: string;
-    };
-    interests: string[];
-    friends: string[]; // array of User ObjectIds (or populated User objects if needed)
-    blockedUsers: string[];
-    lastSeen: string;
-    bio: string;
-    emailVerified: boolean;
-    friendRequests: string[];
-    language: string;
-    isOnline: boolean;
-    lastLogin?: string | null;
-    loginAttempts: number;
-    lockUntil?: string | null;
-    createdAt: string;
-    updatedAt: string;
-}
-
-export interface UserState {
   id: string;
+  email: string;
   fullName: string;
   username: string;
-  email: string;
-  token: string;
   authProvider: "google" | "github" | "local";
+  password?: string;
   birthday?: string | null;
-  location: {
-    country?: string;
-    city?: string;
-  };
   avatar: {
     url: string;
     public_id?: string;
   };
+  location: {
+    country?: string;
+    city?: string;
+  };
   interests: string[];
+  friends: string[];
+  blockedUsers: string[];
+  lastSeen: string;
+  bio: string;
+  emailVerified: boolean;
+  friendRequests: string[];
   language: string;
+  isOnline: boolean;
+  lastLogin?: string | null;
+  loginAttempts: number;
+  lockUntil?: string | null;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface UserState extends Partial<User> {
+  token?: string;
   isAuthenticated: boolean;
 }
